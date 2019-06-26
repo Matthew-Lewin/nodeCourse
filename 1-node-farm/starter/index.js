7fa3340e8b9cd2ba376b564a32a0b8c6ec0d3f3a
Matthew <contact@lewin.io>
2019-06-26 18:06:30 -0500

    Learn to set up a basic server.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,35 @@
-const fs = require('fs');
+
+//Synchronous Way
+/* const fs = require('fs');
 const input = fs.readFileSync('./txt/input.txt', 'utf-8');
 const output = `Food is amazing. ${input}\nNow I want an avocado!`;
 fs.writeFileSync('./txt/output.txt', output);
+ */
+
+
+ //File System Stuff!
+/*  const fs = require('fs');
+
+ fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
+            fs.writeFile('./txt/output.txt', (`${data2}\n${data3}`), 'utf-8', error => {
+                console.log('Your file is ready my lord.');
+            });
+        });
+    });
+ });
+ console.log("let's read some data!"); */
+
+ //Server stuff
+
+ const http = require('http');
+ 
+ const server = http.createServer((req, res) => {
+     res.end('Here - have a server.');
+ });
+
+ server.listen(8000, '127.0.0.1', () => {
+     console.log('Server is running on port 8000.');
+ });
