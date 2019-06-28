99e6fa570cfc44822840f0587c59bd295ccffdaf
Matthew <contact@lewin.io>
2019-06-27 22:11:27 -0500

    Begin creating API.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -26,12 +26,19 @@ fs.writeFileSync('./txt/output.txt', output);
 
  const http = require('http');
  const url = require('url');
+ const fs = require('fs');
  
  const server = http.createServer((req, res) => {
      if (req.url === '/' || req.url === '/overview') {
          res.end('This is the overview.');
      } else if (req.url === '/product') {
          res.end('This it the product.');
+     } else if (req.url === '/api') {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data);
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data);
+        })
      } else {
          res.writeHead(404, {"Content-type" : "text/html"});
          res.end("<h1>The page you are looking for ain't here</h1>");
