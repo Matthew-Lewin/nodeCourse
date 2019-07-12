cd13114adb65cf761d8a5e47c7863e0251cde0a3
Matthew <contact@lewin.io>
2019-07-12 17:58:26 -0500

    Change code to use promises.

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("File not found.");
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+
+    superagent
+      .get(`https://dog.ceo/api/breed/${data}/images/random`)
+      .then(res => {
+        console.log(res.body.message);
+
+        fs.writeFile("dog-image.txt", res.body.message, err => {
+          if (err) return console.log(err.message);
+          console.log("Random dog image saved to file!");
+        });
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  })
+  .catch(err => {
+    console.log(err);
+  });
