70c483ce7e272333edb96789cb78e47d42406c20
Matthew <contact@lewin.io>
2019-07-16 19:04:27 -0500

    Handle url paramaters

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -18,6 +18,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID. 🙁'
+    });
+  }
+
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   /* console.log(req.body);
   res.send('Done. 😉'); */
@@ -31,7 +52,7 @@ app.post('/api/v1/tours', (req, res) => {
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     err => {
-      res.status(200).json({
+      res.status(201).json({
         status: 'success',
         data: {
           tour: newTour
