444a7a7319e905dc671ea85cc7c8ac39639b3fea
Matthew <contact@lewin.io>
2019-08-03 16:26:32 -0500

    Connected app to DB

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,24 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('Connection to DB was successful');
+  });
+
 const app = require('./app');
-console.log(process.env);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
