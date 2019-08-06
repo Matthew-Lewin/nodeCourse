84ff8dce4566d79b23c49729267b321c85099c02
Matthew <contact@lewin.io>
2019-08-05 19:04:25 -0500

    Finish implementing basic CRUD functions.

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -72,9 +72,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).send({
+      status: 'fail',
+      message: err
+    });
+  }
 };
--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -6,14 +6,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'Name is required'],
     unique: true
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Duration is required']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'MaxGroupSize is required']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Difficulty is required']
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
     required: [true, 'Price is required']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have a cover image']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: true
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
