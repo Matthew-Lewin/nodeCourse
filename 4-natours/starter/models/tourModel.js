cf5e461efdc4eb4a7c0f9e5cc82927d37b932d64
Matthew Lewin <contact@lewin.io>
2019-10-03 18:42:28 -0500

    Add validators to tourSchema model. This includes both custom and mongoose validators.

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -7,7 +8,14 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Name is required'],
       unique: true,
-      trim: true
+      trim: true,
+      maxlength: [
+        40,
+        'A tour name must have less than or equal to 40 characters'
+      ],
+      minlength: [10, 'A tour name must have 10 or more characters']
+      /*       validate: [validator.isAlpha, 'Tour name must only contain characters'] */
+      // Above excludes spaces, too... 💀
     },
     slug: String,
     duration: {
@@ -20,11 +28,18 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'Difficulty is required']
+      required: [true, 'Difficulty is required'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, medium or difficult'
+        //enum is for strings only!
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -34,7 +49,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'Price is required']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // this only points to current doc when creating NEW documents
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be less than regular price'
+      }
+    },
     summary: {
       type: String,
       trim: true,
@@ -76,19 +100,37 @@ tourSchema.pre('save', function() {
   next();
 });
 
-tourSchema.pre('save', function(next) {
-  console.log('Will save document... 🧾');
-  next();
-});
-
 tourSchema.post('save', function(doc, next) {
   console.log(doc);
   next();
 });
 
+tourSchema.pre('save', function(next) {
+  console.log('Will save document... 🧾');
+
+  next();
+});
+
 // Query Middleware
-tourSchema.pre('find', function(next) {
+/* tourSchema.pre('find', function(next) {
+ */
+tourSchema.pre(/^find/, function(next) {
   this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
+
+//Aggregation Middleware
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 
