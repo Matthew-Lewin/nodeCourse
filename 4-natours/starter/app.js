32f6f0433081c6e0adad68eafdbc0a646e3ed00a
Matthew <contact@lewin.io>
2019-10-05 16:49:04 -0500

    Create error object and implement global error handler.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 
 const app = express();
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -24,27 +26,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
-  /* res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`
-  }); */
-  const err = new Error(`Can't find ${req.originalUrl}`);
-  err.status = 'fail';
-  err.statusCode = 404;
   // if you pass an error to next, Express knows there is an error and skips to error middleware
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
 });
 // Error handling middleware
 // Express knows this is error handler, due to 4 parameters
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 
 // 4) Start server
 
