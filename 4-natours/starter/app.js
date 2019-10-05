04bb7937af3b1b1e628e4c63cffe71b01686b2e0
Matthew <contact@lewin.io>
2019-10-05 16:30:42 -0500

    Begin implement better error handling in app.js

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -24,9 +24,25 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
-  res.status(404).json({
+  /* res.status(404).json({
     status: 'fail',
     message: `Can't find ${req.originalUrl}`
+  }); */
+  const err = new Error(`Can't find ${req.originalUrl}`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  // if you pass an error to next, Express knows there is an error and skips to error middleware
+  next(err);
+});
+// Error handling middleware
+// Express knows this is error handler, due to 4 parameters
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
