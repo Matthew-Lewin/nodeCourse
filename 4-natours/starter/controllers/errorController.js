9b8287fabebf5e3939571e07141bf6dcb2f99985
Matthew Lewin <contact@lewin.io>
2019-10-06 16:58:25 -0500

    Implement differentiation between production and development environments for error reporting.

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,17 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  //operational errors, trusted errors (often user errors)
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    //programming errors, stuff you'd rather user not know
+  } else {
+    // Log the error
+    console.error('ERROR 😢', err);
+
+    // Send generic error to client
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong... 💀'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-      stack: err.stack
-    });
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
+    sendErrorProd(err, res);
   }
 };
